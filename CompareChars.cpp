976a495f410c4d6ff0518a457c67a26329114da9
SHYAM KHADE <khade.shyam10.1@gmail.com>
2023-02-07 18:23:14 +0530

    learn charactor comparision

---
+++ CompareChars.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+cout << ('A' < 'a') <<endl; // Output: 1
+cout << ('Z' < 'a') <<endl; // Output: 1
+cout << ('Z' > 'a') <<endl; // Output: 0
+
+
+    return 0;
+}
+
+
