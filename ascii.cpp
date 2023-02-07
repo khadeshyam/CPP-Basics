bc19f651ee3b89eee0602e4bcb56a513c21c9922
SHYAM KHADE <khade.shyam10.1@gmail.com>
2023-02-07 18:06:30 +0530

    learn conversions of charectors (Uppercase,Lowercase)

--- ascii.cpp
+++ ascii.cpp
@@ -5,4 +5,27 @@ int main()
 {
     char ch = 'A';
     cout<<"ASCII value of "<<ch<<" is "<<int(ch)<<endl;
+
+    ch = 'a';
+    cout<<"ASCII value of "<<ch<<" is "<<int(ch)<<endl;
+
+    ch = 'B';
+    cout<<"ASCII value of "<<ch<<" is "<<int(ch)<<endl;
+
+    ch = 'b';
+    cout<<"ASCII value of "<<ch<<" is "<<int(ch)<<endl;
+
+    cout<<"difference of charactors (a , A) "<<'a'-'A'<<endl;
+    cout<<"difference of charactors (b , B) "<<'a'-'A'<<endl;
+    cout<<"difference of charactors (c , C) "<<'c'-'C'<<endl;
+
+    cout<<"difference of charactors ( A, a ) "<<'A'-'a'<<endl;
+    cout<<"difference of charactors ( B, b ) "<<'B'-'b'<<endl;
+    cout<<"difference of charactors ( C, c ) "<<'C'-'c'<<endl;
+
+    cout<<"convert B to Lowercase "<<char('B'+('a'-'A'))<<endl;
+    cout<<"convert b to Uppercase "<<char('b'+('A'-'a'))<<endl;
+
+
+
 }
